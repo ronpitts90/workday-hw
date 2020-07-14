d409212310f0d99025f041ef802fdd505b3c3609
Ronald Pitts <ronpitts81@gmail.com>
2020-07-14 19:14:38 -0400

    adding updated hw for plan

---
+++ scripts.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    // listen for saves
+    $(".saveBtn").on("click", function() {
+        // get nearby values 
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // local storage
+        localStorage.setItem(time,value);
+    }); 
+
+function hourUpdater() {
+    // get current number of hours
+    var currentHour = moment().hours();
+
+    // loop over time blocks 
+    $(".time-block").each(function(){
+        var blocker = parseInt($(this).attr("id").split("")[1]);
+
+        // check if moved past this time 
+        if (blocker < currentHour){
+            $(this).addClass("past");
+        }
+        else if (blocker === currentHour) {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+        }
+        else {
+            $(this).removeClass("past");
+            $(this).removeClass("present"); 
+            $(this).addClass("future");
+        }
+    });
+}
+
+hourUpdater();
+
+// set up interval to check it time needs to be updated 
+var interval = setInterval(hourUpdater, 15000);
+
+// load any saved data from localStorage 
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16.description").val(localStorage.getItem("hour-16"));
+$("#hour-17.description").val(localStorage.getItem("hour-17"));
+
+// show current day 
+$("#currentDay").text(moment().format)("dddd,MMMM Do");
